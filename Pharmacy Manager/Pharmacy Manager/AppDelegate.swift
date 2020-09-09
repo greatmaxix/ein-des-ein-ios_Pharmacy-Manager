d76c3afb58272713a03e65e6f35402378e62a7d7
Tymoshchenko Mykhailo <mtymo@softserveinc.com>
2020-09-09 16:37:28 +0300

    Initial Commit

---
+++ Pharmacy Manager/Pharmacy Manager/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Pharmacy Manager
+//
+//  Created by Mikhail Timoscenko on 09.09.2020.
+//  Copyright © 2020 PharmacyManager. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Pharmacy Manager/Pharmacy Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pharmacy Manager
+//
+//  Created by Mikhail Timoscenko on 09.09.2020.
+//  Copyright © 2020 PharmacyManager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
