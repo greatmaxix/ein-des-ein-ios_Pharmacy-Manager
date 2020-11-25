5ea702b68e561dff3d95a813e8a11d37aca7ea75
Egor Bozko <egorbzk@gmail.com>
2020-11-25 22:50:35 +0200

    User image uploading

--- Pharmacy Manager/Pharmacy Manager/Flows/Chat/ViewControllers/Chat/ChatModel.swift
+++ Pharmacy Manager/Pharmacy Manager/Flows/Chat/ViewControllers/Chat/ChatModel.swift
@@ -347,7 +347,7 @@ final class ChatModel: Model, ChatInput {
     private func recursiveUpload(images: [LibraryImage]) {
         if let image = images.first, let data = image.original.jpegData(compressionQuality: 0.5) {
             let mime = "image/\(image.url?.lastPathComponent.components(separatedBy: ".").last ?? "jpg")"
-            uploadProvider.load(target: .upload(data: data, mime: mime, name: image.url?.lastPathComponent ?? "image")) {[weak self] result in
+            uploadProvider.load(target: .uploadUser(data: data, mime: mime, name: image.url?.lastPathComponent ?? "image")) {[weak self] result in
                 self?.output?.uploadFinished(image: image, with: result)
                 switch result {
                 case .success(let response):
